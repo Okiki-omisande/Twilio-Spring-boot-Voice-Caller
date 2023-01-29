d3e0ff3ff9ff12b8bbf8ff3451ad85b0a8e5076a
= <okikiomisande8@gmail.com>
2023-01-29 23:29:48 +0100

    Removed Personal Details

--- src/main/java/com/example/SpringbootTwiliovoicecall/SpringBootTwilioVoiceCallApplication.java
+++ src/main/java/com/example/SpringbootTwiliovoicecall/SpringBootTwilioVoiceCallApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootTwilioVoiceCallApplication {
+public class SpringBootTwilioVoiceCallApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootTwilioVoiceCallApplication.class, args);
+		SpringApplication.run(SpringBootTwilioVoiceCallApplication.class, args);
 	}
 
 }
--- src/main/java/com/example/SpringbootTwiliovoicecall/TwilioConfig.java
+++ src/main/java/com/example/SpringbootTwiliovoicecall/TwilioConfig.java
@@ -5,10 +5,10 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class TwilioConfig {
-    public static String MY_ACCT_SID= "AC6398478828dcf8701e9cb410f05a7291";
-    public static String MY_AUTH_TOKEN = "69006dfca6d645f3108c4c2e5a1bf8b6";
-    public static String MY_TWILIO_PHONE_NUMBER = "+14632634373";
-    public static String VC_RECEIVING_NUMBER = "+2347087870938";
+    public static String MY_ACCT_SID= "";
+    public static String MY_AUTH_TOKEN = "";
+    public static String MY_TWILIO_PHONE_NUMBER = "";
+    public static String VC_RECEIVING_NUMBER = "";
 
     static {
         Twilio.init(MY_ACCT_SID,MY_AUTH_TOKEN);
